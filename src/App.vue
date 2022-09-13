20e8f79030cbdc77f65d418002403fbec7a17515
Krystian Głuszko <gluszkokrystian@gmail.com>
2022-09-13 21:17:12 +0200

    splitting modal functionality across components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <script setup>
 import moviesTableVue from "./components/moviesTable.vue";
 import headerVue from "./components/header.vue";
-import movieModalVue from "./components/movieModal.vue";
 import { inject } from "vue";
-import MovieModal from "./components/movieModal.vue";
 
 // getList();
 </script>
