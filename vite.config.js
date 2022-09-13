9f7aa62c951450aa9507854212203b8a67873392
Krystian Głuszko <gluszkokrystian@gmail.com>
2022-09-13 21:22:37 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,6 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   //disabling proxy for production
-  publicDir: "",
-  server: {
     open: true,
     proxy: {
       "/api": {
